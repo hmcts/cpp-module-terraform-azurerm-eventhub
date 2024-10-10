a6afb093102827c0fe7397e4f2d2b450aff89522
Jyothi Balla <jyothi.balla@hmcts.net>
2024-10-10 17:51:58 +0100

    test

--- main.tf
+++ main.tf
@@ -1,12 +1,41 @@
-resource "azurerm_eventhub_namespace" "main" {
-  name                          = var.eventhub_namespace_name
-  location                      = var.location
-  resource_group_name           = var.resource_group_name
-  sku                           = var.sku
-  capacity                      = var.capacity
-  auto_inflate_enabled          = var.auto_inflate_enabled
-  maximum_throughput_units      = !var.auto_inflate_enabled ? "0" : var.maximum_throughput_units
-  public_network_access_enabled = var.public_network_access_enabled
+locals {
+  consumers = { for hc in flatten([for h in var.hubs :
+    [for c in h.consumers : {
+      hub  = h.name
+      name = c
+    }]]) : format("%s.%s", hc.hub, hc.name) => hc }
+
+  keys = { for hk in flatten([for h in var.hubs :
+    [for k in h.keys : {
+      hub = h.name
+      key = k
+    }]]) : format("%s.%s", hk.hub, hk.key.name) => hk }
+
+  hubs                = { for h in var.hubs : h.name => h }
+  authorization_rules = { for a in var.authorization_rules : a.name => a }
+
+
+}
+
+
+resource "azurerm_eventhub_namespace" "eventhub" {
+  name                = var.eventhub_namespace_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku                  = var.sku
+  capacity             = var.capacity
+  auto_inflate_enabled = var.auto_inflate_enabled
+
+
+  dynamic "identity" {
+    for_each = var.identity == {} ? [] : [var.identity]
+    content {
+      type         = lookup(identity.value, "type", null)
+      identity_ids = lookup(identity.value, "identity_ids", null)
+    }
+  }
+
+  maximum_throughput_units = var.maximum_throughput_units
 
   dynamic "network_rulesets" {
     for_each = var.network_rulesets
@@ -30,13 +59,59 @@ resource "azurerm_eventhub_namespace" "main" {
       }
     }
   }
+
+  local_authentication_enabled  = var.local_authentication_enabled
+  public_network_access_enabled = var.public_network_access_enabled
+  minimum_tls_version           = var.minimum_tls_version
+
   tags = var.tags
 }
 
-resource "azurerm_eventhub" "main" {
-  name                = var.eventhub_name
-  namespace_name      = azurerm_eventhub_namespace.main.name
+resource "azurerm_eventhub" "events" {
+  for_each = local.hubs
+
+  name                = each.key
+  namespace_name      = azurerm_eventhub_namespace.eventhub.name
   resource_group_name = var.resource_group_name
-  partition_count     = var.partition_count
-  message_retention   = var.message_retention
+  partition_count     = each.value.partitions
+  message_retention   = each.value.message_retention
+}
+
+
+resource "azurerm_eventhub_namespace_authorization_rule" "events" {
+  for_each = local.authorization_rules
+
+  name                = each.key
+  namespace_name      = azurerm_eventhub_namespace.eventhub.name
+  resource_group_name = var.resource_group_name
+
+  listen = each.value.listen
+  send   = each.value.send
+  manage = each.value.manage
+}
+
+resource "azurerm_eventhub_consumer_group" "events" {
+  for_each = local.consumers
+
+  name                = each.value.name
+  namespace_name      = azurerm_eventhub_namespace.eventhub.name
+  eventhub_name       = each.value.hub
+  resource_group_name = var.resource_group_name
+
+  depends_on = [azurerm_eventhub.events]
+}
+
+resource "azurerm_eventhub_authorization_rule" "events" {
+  for_each = local.keys
+
+  name                = each.value.key.name
+  namespace_name      = azurerm_eventhub_namespace.eventhub.name
+  eventhub_name       = each.value.hub
+  resource_group_name = var.resource_group_name
+
+  listen = each.value.key.listen
+  send   = each.value.key.send
+  manage = each.value.key.manage
+
+  depends_on = [azurerm_eventhub.events]
 }
--- variables.tf
+++ variables.tf
@@ -139,3 +139,49 @@ variable "public_network_access_enabled" {
   default     = true
   type        = bool
 }
+
+variable "local_authentication_enabled" {
+  description = "(Optional) Is SAS authentication enabled for the EventHub Namespace?"
+  default     = true
+  type        = bool
+}
+
+variable "minimum_tls_version" {
+  description = "(Optional) The minimum supported TLS version for this EventHub Namespace. Valid values are: 1.0, 1.1 and 1.2"
+  default     = "1.2"
+  type        = string
+}
+
+variable "identity" {
+  description = "Identity block Specifies the identity to assign to function app"
+  type        = any
+  default     = {}
+}
+
+variable "authorization_rules" {
+  description = "Authorization rules to add to the namespace. For hub use `hubs` variable to add authorization keys."
+  type = list(object({
+    name   = string
+    listen = bool
+    send   = bool
+    manage = bool
+  }))
+  default = []
+}
+
+variable "hubs" {
+  description = "A list of event hubs to add to namespace."
+  type = list(object({
+    name              = string
+    partitions        = number
+    message_retention = number
+    consumers         = list(string)
+    keys = list(object({
+      name   = string
+      listen = bool
+      send   = bool
+      manage = bool
+    }))
+  }))
+  default = []
+}
